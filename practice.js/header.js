36c2229855299de6b0b6d0284a4fa4dbe62b34ae
hammadhussain1234 <122485603+hammadhussain1234@users.noreply.github.com>
2023-07-06 10:08:20 -0700

    Advantureland

---
+++ practice.js/header.js
@@ -0,0 +1,25 @@
+document.getElementById("header").innerHTML=`
+<nav class="navbar navbar-expand-lg">
+<div class="container-fluid">
+  <a class="navbar-brand  text-light"><img src="image/download.png2-removebg-preview.png" width="200px" alt=""></a>
+  <button class="navbar-toggler " type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation ">
+    <span class="navbar-toggler-icon "><i class="fa-solid fa-bars"></i></span>
+  </button>
+  <div class="collapse navbar-collapse ms-auto" id="navbarSupportedContent">
+    <ul class="navbar-nav ms-auto " style="font-size: 30px; font-weight: bold;">
+      <li class="nav-item ">
+        <a class="nav-link active "href="Home.html" id="home">Home</a>
+      </li>
+      <li class="nav-item ">
+        <a class="nav-link active"href="about.html" id="about">About</a>
+      </li>
+      <li class="nav-item ">
+      <a class="nav-link active"href="Services.html" id="services">Services</a>
+    </li>
+      <li class="nav-item ">
+        <a class="nav-link " href="Contact.html" id="contact">Contact</a>
+      </li>
+    </ul>
+  </div>
+</div>
+</nav>`
